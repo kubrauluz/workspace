38ce9ca54503e066c0a05a94aaff972e540fd88f
kubrauluz <kubrauluz@altamira.com.tr>
2022-11-23 15:41:33 +0300

    fixed

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import {ProfileComponent} from "./profile.component";
-import {MatTableModule} from "@angular/material/table";
 import { BalancesComponent } from './balances/balances.component';
 import { OpenOrdersComponent } from './open-orders/open-orders.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
-import {MatTabsModule} from "@angular/material/tabs";
 
 
 @NgModule({
@@ -19,9 +17,7 @@ import {MatTabsModule} from "@angular/material/tabs";
   ],
   imports: [
     CommonModule,
-    ProfileRoutingModule,
-    MatTableModule,
-    MatTabsModule
+    ProfileRoutingModule
   ]
 })
 export class ProfileModule { }
